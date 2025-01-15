fd2ef12e191ed5d3f6bb36b283611d332f209394
ethandelb <ethandelbiaggiobelli@gmail.com>
2025-01-15 17:52:50 +0000

    Aggiunta bottone di scarto

--- app.py
+++ app.py
@@ -14,13 +14,15 @@ if 'deck' not in st.session_state:
     number_of_decks = st.number_input("Number of decks", min_value=1, max_value=10, value=1)
     st.session_state['deck'] = Deck(number_of_decks)
     st.session_state['drawn_cards'] = []
+    st.session_state['selected_cards'] = []
 
 start_button = st.button("Start")
 if start_button:
     st.session_state['deck'].shuffle()
     st.session_state['drawn_cards'] = []
+    st.session_state['selected_cards'] = []
 
-
+Scarta = st.button("scarta")
 
 columns = st.columns(carte_mano)
 if 'drawn_cards' not in st.session_state:
@@ -36,11 +38,22 @@ for i in range(carte_mano):
 
         st.image(card.image, use_container_width=True)
         if st.button(f"carta {i + 1}", use_container_width=True):
-            card.selected = True
+            if card not in st.session_state['selected_cards']:
+                st.session_state['selected_cards'].append(card)
+                card.selected = True
+            else:
+                st.session_state['selected_cards'].remove(card)
+                card.selected = False
         if card.selected == True:
             st.write("Carta selezionata",use_container_width=True)
+                
 
-    
+if Scarta:
+    for card in st.session_state['selected_cards']:
+        if card in st.session_state['drawn_cards']:
+            st.session_state['drawn_cards'].remove(card)
+    st.session_state['selected_cards'] = []
+
 
 
 
